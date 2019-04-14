6485b3da44996ec1ee174bf21f93a95ab408f6b9
tsv <todor.stoyanov@oru.se>
2019-04-14 20:16:25 +0200

    updated script

--- panda_table_launch/scripts/sim_nominal_joints.sh
+++ panda_table_launch/scripts/sim_nominal_joints.sh
@@ -6,7 +6,7 @@ rosservice call /hiqp_joint_effort_controller/set_tasks \
   active: 1
   monitored: 1
   def_params: ['TDefFullPose', '2.007', '1.77', '0.007', '-2.35', '0.007', '2.1', '0.9']  
-  dyn_params: ['TDynPD', '10.0', '7.0'] "
+  dyn_params: ['TDynPD', '200.0', '29.0'] "
 
 #def_params: ['TDefFullPose', '0.0', '-1.17', '0.003', '-2.89', '-0.0', '1.82', '0.84']
   #def_params: ['TDefFullPose', '-0.001', '-0.476', '0.003', '-2.654', '-0.0', '2.236', '0.84']
