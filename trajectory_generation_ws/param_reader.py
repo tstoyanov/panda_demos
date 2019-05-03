9b9037a6e5f57e93a7c7627eb2849b6ed6e59ca0
Tommaso Castagna <tttt@oru.se>
2019-05-03 09:36:31 +0200

    Solved orientation problem and workspace cleanup

--- trajectory_generation_ws/param_reader.py
+++
@@ -1,9 +0,0 @@
-import rospy
-
-param_server_joints = "/position_joint_trajectory_controller/joints"
-
-joint_names = rospy.get_param(param_server_joints)
-
-print("type: ", type(joint_names))
-print("type(type): ", type(joint_names[0]))
-print("joint_names: ", joint_names)
