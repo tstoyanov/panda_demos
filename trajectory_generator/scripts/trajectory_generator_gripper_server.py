fb13e1df996d5b24ac179cb5ea19fc27e454b3f6
Tommaso <castagna.tommaso@gmail.com>
2019-08-20 17:05:56 +0200

    tests on grippet service

--- trajectory_generator/scripts/trajectory_generator_gripper_server.py
+++ trajectory_generator/scripts/trajectory_generator_gripper_server.py
@@ -3,15 +3,15 @@
 from trajectory_generator.srv import *
 import rospy
 
-def handle_trajectory_generetor_gripper(req):
+def handle_trajectory_generator_gripper(req):
     print "Returning [%s + %s = %s]"%(req.a, req.b, (req.a + req.b))
     return trajectory_generator_gripperResponse(req.a + req.b)
 
-def trajectory_generetor_gripper_server():
-    rospy.init_node('trajectory_generetor_gripper_server')
-    s = rospy.Service('trajectory_generetor_gripper', trajectory_generator_gripper, handle_trajectory_generetor_gripper)
+def trajectory_generator_gripper_server():
+    rospy.init_node('trajectory_generator_gripper_server')
+    s = rospy.Service('trajectory_generator_gripper_server', trajectory_generator_gripper, handle_trajectory_generator_gripper)
     print "Ready to add two ints."
     rospy.spin()
 
 if __name__ == "__main__":
-    trajectory_generetor_gripper_server()
+    trajectory_generator_gripper_server()
