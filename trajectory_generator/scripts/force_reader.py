4fc4580e864fae8b1dd40e6c9cdf174ef5fa31f2
Tommaso <castagna.tommaso@gmail.com>
2019-09-06 16:49:14 +0200

    continuation of work on force reading

--- trajectory_generator/scripts/force_reader.py
+++ trajectory_generator/scripts/force_reader.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import sys
 import json
+import copy
 import rospy
 import argparse
 
@@ -10,17 +11,35 @@ from rospy_message_converter import json_message_converter
 import pprint
 pp = pprint.PrettyPrinter(indent=2)
 
+g = 9.807
+
 import matplotlib.pyplot as plt
+import numpy as np
+x = []
+y = []
+
+# You probably won't need this if you're embedding things in a tkinter plot...
+plt.ion()
+
 fig = plt.figure()
-ax = fig.add_subplot(111)
-line1, = ax.plot(0, 0, 'r-')
-
-
+ax1 = fig.add_subplot(211)
+ax1.set_ylabel('z force')
+line11, = ax1.plot(x, y, 'r-', label="low pass filter (a = 0.99)", alpha=0.7, linewidth=2) # Returns a tuple of line objects, thus the comma
+line12, = ax1.plot(x, y, 'b-', label="sample mean", alpha=0.7, linewidth=2)
+# line13, = ax1.plot(x, y, 'k-', label="new data", marker="o", alpha=0.7)
+ax1.legend(loc='upper center', bbox_to_anchor=(0.5, 1.07), ncol=2, fancybox=True, shadow=True)
+ax2 = fig.add_subplot(212)
+ax2.set_ylabel('last sample')
+# line21, = ax2.plot(x, y, 'r-', label="dumb mean", alpha=0.7) 
+# line22, = ax2.plot(x, y, 'b-', label="sample mean", alpha=0.7) 
+line23, = ax2.plot(x, y, 'k-', label="new data", marker="o", alpha=0.7)
+ax2.legend(loc='upper center', bbox_to_anchor=(0.5, 1.07), ncol=2, fancybox=True, shadow=True)
 
 # calulates the mean, population variance and sample variance every "batch_size" data points
 class statistics_calculator:
-    def __init__(self, batch_size=300, topic="/panda/franka_state_controller/F_ext", message_type=WrenchStamped):
+    def __init__(self, batch_size=100, topic="/panda/franka_state_controller/F_ext", message_type=WrenchStamped):
 
+        self.batch_count = 0
         self.dumb_count = 0
         self.dumb_mean = {
             "force": {
@@ -86,6 +105,19 @@ class statistics_calculator:
                 "z": 0
             }
         }
+        self.last_sample = {
+            "force": {
+                "x": 0,
+                "y": 0,
+                "z": 0
+            },
+            "torque": {
+                "x": 0,
+                "y": 0,
+                "z": 0
+            }
+        }
+        
 
     # for a new value new_value, compute the new count, new mean, the new M2.
     # mean accumulates the mean of the entire dataset
@@ -94,19 +126,23 @@ class statistics_calculator:
     # after updating "batch_size" amount of times calculates
     # the population and sample variance and resets the accumulators
     def update(self, new_value):
+        self.last_sample = copy.deepcopy(new_value)
         if self.count == 0:
-            self.mean = new_value
-            self.dumb_mean = new_value
+            self.mean = copy.deepcopy(new_value)
+        
         # (count, mean, M2) = existing_aggregate
         self.count += 1
-
+        self.batch_count += 1
+        
+        if self.dumb_count == 0:
+            self.dumb_mean = copy.deepcopy(new_value)
         self.dumb_count += 1
         for key, coordinates in new_value.iteritems():
             for coordinate in coordinates.keys():
                 coordinate = str(coordinate)
                 # self.dumb_mean[key][coordinate] = ((self.dumb_count-1)*(self.dumb_mean[key][coordinate]) + new_value[key][coordinate]) / self.dumb_count
                 self.dumb_mean[key][coordinate] = 0.99*(self.dumb_mean[key][coordinate]) + 0.01*(new_value[key][coordinate])
-        line1.set_ydata(new_value["force"]["z"])
+                # self.dumb_mean[key][coordinate] = new_value[key][coordinate]
         # print ("dumb mean:")
         # pp.pprint(self.dumb_mean)
 
@@ -126,7 +162,7 @@ class statistics_calculator:
         # delta2 = new_value - self.mean
         # self.M2 += delta * delta2
 
-        if self.count >= self.batch_size:
+        if self.batch_count >= self.batch_size:
             for key, coordinates in self.mean.iteritems():
                 for coordinate in coordinates.keys():
                     self.variance[key][coordinate] = self.M2[key][coordinate]/self.count
@@ -142,7 +178,7 @@ class statistics_calculator:
             # print ("mean = ", self.mean)
             # print ("polulation variance = ", self.variance)
             # print ("sample variance = ", self.sample_variance)
-            self.count = 0
+            self.batch_count = 0
         
 
         # return (count, mean, M2)
@@ -170,7 +206,42 @@ def main(args):
     rospy.init_node('force_reader', anonymous=True)
     stats = statistics_calculator(topic="/panda/franka_state_controller/F_ext", message_type=WrenchStamped)
     try:
+        rospy.rostime.wallsleep(1)
+        start = rospy.get_rostime()
+        while (start.secs == 0 | start.nsecs == 0):
+            start = rospy.get_rostime()
+            # start = start.to_sec()
         while not rospy.core.is_shutdown():
+            rospy.rostime.wallsleep(0.1)
+            
+            line11.set_xdata(np.append(line11.get_xdata(), stats.dumb_count))
+            line11.set_ydata(np.append(line11.get_ydata(), stats.dumb_mean["force"]["z"]))
+            line12.set_xdata(np.append(line12.get_xdata(), stats.dumb_count))
+            line12.set_ydata(np.append(line12.get_ydata(), stats.mean["force"]["z"]))
+            # line13.set_xdata(np.append(line13.get_xdata(), stats.dumb_count))
+            # line13.set_ydata(np.append(line13.get_ydata(), stats.last_sample["force"]["z"]))
+            # line21.set_xdata(np.append(line21.get_xdata(), stats.dumb_count))
+            # line21.set_ydata(np.append(line21.get_ydata(), stats.dumb_mean["force"]["x"]))
+            # line22.set_xdata(np.append(line22.get_xdata(), stats.dumb_count))
+            # line22.set_ydata(np.append(line22.get_ydata(), stats.mean["force"]["x"]))
+            # line23.set_xdata(np.append(line23.get_xdata(), stats.dumb_count))
+            # line23.set_ydata(np.append(line23.get_ydata(), stats.last_sample["force"]["x"]))
+            line23.set_xdata(np.append(line23.get_xdata(), stats.dumb_count))
+            line23.set_ydata(np.append(line23.get_ydata(), stats.last_sample["force"]["z"]))
+
+            now = rospy.get_rostime()
+
+            # ax1.text(1, 0.65, "estimated lpf mass = " + str(stats.dumb_mean["force"]["z"]/g))
+            # ax1.text(2, 0.65, "estimated sample mass = " + str(stats.mean["force"]["z"]/g))
+            print ("Elapsed time: " + str(now.to_sec() - start.to_sec()))
+            print ("Estimated lpf mass = " + str(stats.dumb_mean["force"]["z"]/g*1000) + " [g]")
+            print ("Estimated sample mass = " + str(stats.mean["force"]["z"]/g*1000) + " [g]")
+            print ("Delta = " + str(abs((stats.dumb_mean["force"]["z"]/g*1000)-(stats.mean["force"]["z"]/g*1000))) + " [g]\n")
+
+            ax1.relim()
+            ax1.autoscale_view()
+            ax2.relim()
+            ax2.autoscale_view()
             fig.canvas.draw()
             fig.canvas.flush_events()
         # rospy.spin()
