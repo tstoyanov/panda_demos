0761b5ed1618c73e330989446e4fdd5fb0aec658
Tommaso <castagna.tommaso@gmail.com>
2019-10-12 19:16:11 +0200

    force sensors readings implementation

--- trajectory_generator/scripts/stone_sensing.py
+++ trajectory_generator/scripts/stone_sensing.py
@@ -14,12 +14,69 @@ import moveit_commander
 import getopt
 import os
 
+import thread
+import pandas as pd
+
 from franka_gripper.srv import *
+
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 import rospkg
 rospack = rospkg.RosPack()
 
-def sense_stone(input_folder="latest", tot_time_nsecs=9000000000, is_simulation=False, is_learning=False, t=None):
+def subsample(samples_list, samples_nr):
+    real_sample_interval = len(samples_list)/float(samples_nr)
+    current_sample_interval = int(real_sample_interval)
+    sample_interval_avg = None
+    sample_index = 0
+    subsamples = []
+    while len(subsamples) < 100:
+        if len(subsamples) != 0 and sample_index >= len(samples_list):
+            sample_index = len(subsamples)-1
+        subsamples.append(samples_list[sample_index])
+        if sample_index != 0:
+            sample_interval_avg = (sample_index / float(len(subsamples)-1))
+            if sample_interval_avg > real_sample_interval:
+                current_sample_interval -= 1
+            elif sample_interval_avg < real_sample_interval:
+                current_sample_interval += 1
+        sample_index += current_sample_interval
+    return subsamples
+        
+
+def callback(data, args):
+    sensors_data = args[0]
+    sensors_data["force"]["x"].append(data.wrench.force.x)
+    sensors_data["force"]["y"].append(data.wrench.force.y)
+    sensors_data["force"]["z"].append(data.wrench.force.z)
+    sensors_data["torque"]["x"].append(data.wrench.torque.x)
+    sensors_data["torque"]["y"].append(data.wrench.torque.y)
+    sensors_data["torque"]["z"].append(data.wrench.torque.z)
+
+def listener():
+    rospy.Subscriber("/panda/franka_state_controller/F_ext", geometry_msgs.msg.WrenchStamped, callback, [])
+
+def sense_stone(input_folder="latest", tot_time_nsecs=9000000000, is_simulation=False, is_learning=True, t=None):
+
+    sensors_data = {
+        "force": {
+            "x": [],
+            "y": [],
+            "z": []
+        },
+        "torque": {
+            "x": [],
+            "y": [],
+            "z": []
+        }
+    }
+    rospy.init_node('force_reader', anonymous=True)
+    # thread.start_new_thread(listener, ())
+    
+    # sns.lineplot(range(len(sensors_data["force"]["x"])), sensors_data["force"]["x"])
+    # plt.show()
+
 
     if is_simulation:
         print("SIMULATION MODE")
@@ -68,7 +125,6 @@ def sense_stone(input_folder="latest", tot_time_nsecs=9000000000, is_simulation=
         robot = moveit_commander.RobotCommander(robot_description=robot_desctiption_str, ns=namespace)
         group = moveit_commander.MoveGroupCommander(group_name, robot_description=robot_desctiption_str, ns=namespace)
 
-    waypoints = []
     grasping_point = {
         # x, y, z, w
         "orientation": [-0.9239, 0.3826, -7.02665323908824e-05, 0.0001],
@@ -128,9 +184,17 @@ def sense_stone(input_folder="latest", tot_time_nsecs=9000000000, is_simulation=
 
             # [0.110, 0.034, 1.047]
         ],
-        "joints_values": [
-            [0.22606509396486113, -0.1887067132364415, 0.07615609056239644, -2.2857309241498744, 1.6181217863758401, 1.895162507014846, -1.3307092883330687],
-            
+        "joints_positions": [
+            [0.19445120013816, -0.21487730565614868, 0.04427450973878616, -2.3955809373590724, 0.01187810182718171, 2.180555477353596, -0.5547206810216108],
+
+            [0.3351801368290918, -0.29662777607797913, -0.11244693900398067, -2.3844817042769044, 0.4160744979646716, 1.7924010314720245, -1.3208833060871592],
+            [0.3619200299155657, -0.23360565089133745, -0.12781654499551712, -2.3745498456453022, 0.978863298841051, 2.1175465971498175, -1.117479492057386],
+            [0.30448947731444703, -0.12694879574524728, -0.051233081743954974, -2.3243839509474378, 0.8545456072769047, 2.48963434716984, -0.5527099020836809],
+            [0.15692759708981763, -0.2920189494293214, 0.12201354729948073, -2.3580899385820357, -0.21012455755472154, 1.705037037051065, 0.0049194150333632955],
+            [0.12587268760957215, -0.11777158826903292, 0.16212125076698525, -2.21868566964802, -0.8504195135672374, 1.77099568547142, -0.2145776784027563],
+            [0.14789706793375182, -0.13316567956848385, 0.0641090490445283, -2.301253765842371, -0.7098326927480935, 2.3117149482243375, -0.8717583615683947],
+
+            [0.20444709271878062, -0.2148109244173751, 0.03434020159994296, -2.395439041634496, 0.009049621077046537, 2.1804183384366924, -0.552972294800217]
         ]
     }
 
@@ -166,34 +230,50 @@ def sense_stone(input_folder="latest", tot_time_nsecs=9000000000, is_simulation=
     grasp_pub.publish(grasp_message)
 
     pose_goal = geometry_msgs.msg.Pose()
-    for waypoint_index, _ in enumerate(sensing_waypoints["orientations"]):
-        pose_goal.position.x = sensing_waypoints["positions"][waypoint_index][0]
-        pose_goal.position.y = sensing_waypoints["positions"][waypoint_index][1]
-        pose_goal.position.z = sensing_waypoints["positions"][waypoint_index][2]
-        pose_goal.orientation.x = sensing_waypoints["orientations"][waypoint_index][0]
-        pose_goal.orientation.y = sensing_waypoints["orientations"][waypoint_index][1]
-        pose_goal.orientation.z = sensing_waypoints["orientations"][waypoint_index][2]
-        pose_goal.orientation.w = sensing_waypoints["orientations"][waypoint_index][3]
-
-        waypoints.append(copy.deepcopy(pose_goal))
-
-    (plan, fraction) = group.compute_cartesian_path(
-                                waypoints,   # waypoints to follow
-                                0.01,        # eef_step
-                                0.0)         # jump_threshold
-    
-    display_trajectory = moveit_msgs.msg.DisplayTrajectory()
-    display_trajectory.trajectory_start = robot.get_current_state()
-    display_trajectory.trajectory.append(plan)
-    # Publish
-    display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
-                                               moveit_msgs.msg.DisplayTrajectory,
-                                               queue_size=20)
-    display_trajectory_publisher.publish(display_trajectory);
+    waypoints = []
 
     raw_input("Press enter to start sensing.")
-    group.execute(plan, wait=True)
-    group.stop()
+    for pos in sensing_waypoints["joints_positions"]:
+        joint_goal = group.get_current_joint_values()
+        for joint_index, _ in enumerate(pos):
+            joint_goal[joint_index] = pos[joint_index]
+        group.go(joint_goal, wait=True)
+        group.stop()
+        sub = rospy.Subscriber("/panda/franka_state_controller/F_ext", geometry_msgs.msg.WrenchStamped, callback, [sensors_data])
+        rospy.rostime.wallsleep(1)
+        sub.unregister()
+    # for waypoint_index, _ in enumerate(sensing_waypoints["orientations"]):
+
+    #     waypoints = []
+
+    #     pose_goal.position.x = sensing_waypoints["positions"][waypoint_index][0]
+    #     pose_goal.position.y = sensing_waypoints["positions"][waypoint_index][1]
+    #     pose_goal.position.z = sensing_waypoints["positions"][waypoint_index][2]
+    #     pose_goal.orientation.x = sensing_waypoints["orientations"][waypoint_index][0]
+    #     pose_goal.orientation.y = sensing_waypoints["orientations"][waypoint_index][1]
+    #     pose_goal.orientation.z = sensing_waypoints["orientations"][waypoint_index][2]
+    #     pose_goal.orientation.w = sensing_waypoints["orientations"][waypoint_index][3]
+
+    #     waypoints.append(copy.deepcopy(pose_goal))
+
+    #     (plan, fraction) = group.compute_cartesian_path(
+    #                                 waypoints,   # waypoints to follow
+    #                                 0.01,        # eef_step
+    #                                 0.0)         # jump_threshold
+    
+    #     display_trajectory = moveit_msgs.msg.DisplayTrajectory()
+    #     display_trajectory.trajectory_start = robot.get_current_state()
+    #     display_trajectory.trajectory.append(plan)
+    #     # Publish
+    #     display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
+    #                                             moveit_msgs.msg.DisplayTrajectory,
+    #                                             queue_size=20)
+    #     display_trajectory_publisher.publish(display_trajectory);
+
+    #     raw_input("Press enter to start sensing.")
+    #     group.execute(plan, wait=True)
+    #     group.stop()
+    subset = subsample(samples_list=sensors_data["force"]["x"], samples_nr=100)
 
     raw_input("Press enter to position the arm.")
     pose_goal.position.x = sliding_point["position"][0]
