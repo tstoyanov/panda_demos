83860947a6bad57a0904568505d8b9c85bdcfb2d
Todor Stoyanov <todor.stoyanov@oru.se>
2019-12-04 17:51:21 +0100

    controllers configs and tasks, some tests done

--- panda_table_launch/scripts/sim_nominal_joints.sh
+++ panda_table_launch/scripts/sim_nominal_joints.sh
@@ -1,15 +1,15 @@
 rosservice call /gazebo/unpause_physics "{}"
 #rosservice call /hiqp_model_joint_effort_controller/remove_all_tasks "{}"
 
-rosservice call /hiqp_model_joint_effort_controller/set_tasks \
+rosservice call /hiqp_joint_effort_controller/set_tasks \
 "tasks:
 - name: 'full_pose'
   priority: 2
   visible: 1
   active: 1
   monitored: 1
-  def_params: ['TDefFullPose', '-2.0', '-1.17', '0.003', '-2.89', '-0.0', '1.82', '0.0']
-  dyn_params: ['TDynPD', '9.0', '6.1'] "
+  def_params: ['TDefFullPose', '1.51', '-1.17', '0.003', '-2.89', '-0.0', '1.82', '0.84']
+  dyn_params: ['TDynPD', '0.5', '1.5'] "
 
 #def_params: ['TDefFullPose', '0.0', '-1.17', '0.003', '-2.89', '-0.0', '1.82', '0.84']
   #def_params: ['TDefFullPose', '-0.001', '-0.476', '0.003', '-2.654', '-0.0', '2.236', '0.84']
