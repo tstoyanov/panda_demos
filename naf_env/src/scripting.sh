d0ae5fc53e7d0f31f0996dd83facf6bb39bf4fbd
Quantao Yang <quantao.yang@oru.se>
2020-05-01 22:48:07 +0200

    new episode num

--- naf_env/src/scripting.sh
+++ naf_env/src/scripting.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 clear
-python main.py --num_episodes 5000 --batch_size 8
+python main.py --num_episodes 5000 --batch_size 256
 #python main.py --num_episodes 50000 --batch_size 512
 #python main.py --num_episodes 50000 --batch_size 256
