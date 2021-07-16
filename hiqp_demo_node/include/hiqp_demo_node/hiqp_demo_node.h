3fef24177922d3c223a0a52456ca563fd13a1ab2
Todor Stoyanov <todor.stoyanov@oru.se>
2021-07-16 16:37:13 +0200

    set up skeleton

---
+++ hiqp_demo_node/include/hiqp_demo_node/hiqp_demo_node.h
@@ -0,0 +1,66 @@
+#ifndef HIQP_DEMO_NODE_HH
+#define HIQP_DEMO_NODE_HH
+
+#include <hiqp_ros/hiqp_client.h>
+#include <hiqp_msgs/Primitive.h>
+#include <hiqp_msgs/Task.h>
+
+#include <std_srvs/Empty.h>
+
+#include <Eigen/Core>
+
+namespace hiqp_panda_demo {
+
+///** Abstract interface for a set of tasks and corresponding primitives
+/// must implement methods to get the lists of primitives and tasks that
+/// should be loaded to the client
+class TaskPrimitiveWrapper {
+  public:
+    virtual void getPrimitiveList(std::vector<hiqp_msgs::Primitive> &primitives) = 0;
+    virtual void getTasksList(std::vector<hiqp_msgs::Task> &tasks) = 0;
+};
+
+///**To simplify, a grasp intervall is given as two concentric cylinders,
+/// described by axis v and a point p on the axis (referenced in a static
+/// obj_frame), and two planes. The controller will try to bring endeffector
+/// point e, expressed in frame e_frame, inside the intervall described by the
+/// two cylinders and the planes (i.e., inside the shell formed by the cylinders
+/// and in between the planes described by n^Tx - d = 0)
+class GraspInterval : public TaskPrimitiveWrapper {
+  hiqp_msgs::Primitive upper;
+  hiqp_msgs::Primitive lower;
+  hiqp_msgs::Primitive left;
+  hiqp_msgs::Primitive right;
+  hiqp_msgs::Primitive inner;
+  hiqp_msgs::Primitive outer;
+
+  std::string obj_frame_;  // object frame
+  std::string e_frame_;    // endeffector frame
+  Eigen::Vector3d e_;      // endeffector point expressed in e_frame_
+  float opening_angle;
+    
+  virtual void getPrimitiveList(std::vector<hiqp_msgs::Primitive> &primitives);
+  virtual void getTasksList(std::vector<hiqp_msgs::Task> &tasks);
+
+};
+
+class DemoNode {
+
+  public: 
+    DemoNode();
+  private:
+    unsigned int n_jnts;
+    std::vector<std::string> link_frame_names;
+
+    ros::NodeHandle nh_;
+    ros::NodeHandle n_;
+
+    hiqp_ros::HiQPClient hiqp_client_;
+
+    /// Servers
+    ros::ServiceServer start_demo_srv_;
+    bool startDemo(std_srvs::Empty::Request& req, std_srvs::Empty::Response& res);
+};
+
+}
+#endif
---
+++ hiqp_demo_node/src/hiqp_demo_node.cpp
@@ -0,0 +1,44 @@
+#include <hiqp_demo_node/hiqp_demo_node.h>
+
+using namespace hiqp_panda_demo; 
+
+void GraspInterval::getPrimitiveList(std::vector<hiqp_msgs::Primitive> &primitives) {
+  primitives.clear();
+  primitives.push_back(upper);
+  primitives.push_back(lower);
+  primitives.push_back(left);
+  primitives.push_back(right);
+  primitives.push_back(inner);
+  primitives.push_back(outer);
+}
+
+void GraspInterval::getTasksList(std::vector<hiqp_msgs::Task> &tasks) {
+  tasks.clear();
+}
+
+//Constructor, creates handles and gets stuff off the parameter server
+DemoNode::DemoNode(): n_jnts(7), hiqp_client_("", "hiqp_joint_velocity_controller") {
+  // handle to home
+  nh_ = ros::NodeHandle("~");
+  // global handle
+  n_ = ros::NodeHandle();
+}
+
+//starts up the demo execution. **BLOCKING service**
+bool DemoNode::startDemo(std_srvs::Empty::Request& req, std_srvs::Empty::Response& res) {
+
+  return true;
+}
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "demo_node");
+
+  DemoNode demo_node;
+
+  ROS_INFO("Demo node ready");
+  ros::AsyncSpinner spinner(4);  // Use 4 threads
+  spinner.start();
+  ros::waitForShutdown();
+
+  return 0;
+}
