d0d151cc19a26c8130473a02cbc8635fd7c92c86
dcaceresd <david.caceresdominguez@gmail.com>
2022-02-17 21:40:28 +0100

    Add hiqp_vel launch file

--- panda_table_launch/scripts/vel_space/sim_nominal_joints.sh
+++ panda_table_launch/scripts/vel_space/sim_nominal_joints.sh
@@ -1,9 +1,10 @@
-rosservice call /hiqp_joint_velocity_controller/set_tasks \
+rosservice call /panda/franka_hiqp_joint_velocity_controller/set_tasks \
 "tasks:
 - name: 'full_pose'
   priority: 2
   visible: 1
   active: 1
   monitored: 1
-  def_params: ['TDefFullPose', '-0.51', '-1.17', '0.003', '-2.89', '-0.0', '1.32', '0.84']
-  dyn_params: ['TDynLinear', '0.5'] "
+  def_params: ['TDefFullPose', '0.01', '-1.17', '0.003', '-2.89', '-0.0', '1.82', '0.84']
+  dyn_params: ['TDynPD', '0.5', '1.5'] "
+
